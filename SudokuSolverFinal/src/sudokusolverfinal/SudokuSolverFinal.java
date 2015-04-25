c3607442b964428a5995601803688c88f8fe94a6
rbs4ba <rbspann2@gmail.com>
2015-04-25 19:18:46 -0400

    set up the board and all the values

--- SudokuSolverFinal/src/sudokusolverfinal/SudokuSolverFinal.java
+++ SudokuSolverFinal/src/sudokusolverfinal/SudokuSolverFinal.java
@@ -4,36 +4,133 @@ rian Spann (rbs4ba) & Jessica Ya (jy2fv), Final Theory Project
 package sudokusolverfinal;
 import java.util.Scanner;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class SudokuSolverFinal {
     
-    public static void printBoard(int [][] board, int rows, int cols){
-        System.out.println("- - - - - - - - - - -");
+    public static square [][] board;
+    public static Map<Integer,List<Integer>> conflictsList;
+    
+    
+    public class square{
+        private int value; //value for the square on the board
+        private int position; //decimal val of the position
+        private List<Integer> options;
+        
+        public square(int value, int position, List<Integer> options){
+            this.value = value;
+            this.position = position;
+            this.options = options;
+        }
+        public int getValue(){
+            return value;
+        }
+        public void setValue(int value){
+            this.value = value;
+        }
+        public int getPosition(){
+            return position;
+        }
+        public void setPosition(int position){
+            this.position = position;
+        }
+        public List<Integer> getOptions(){
+            return options;
+        }
+        public void setOptions(List<Integer> options){
+            this.options = options;
+        }
+    }
+    
+    public static void printBoard(square [][] numBoard, int rows, int cols){
+        System.out.println("-------------------");
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(j == 0){
-                    System.out.print("||");
+                    System.out.print("|");
                 }
                 if(j == cols-1){
-                    System.out.print(board[i][j] + "||");
+                    System.out.print(numBoard[i][j].value + "|");
                 } else if(j == 2 || j == 5){
-                    System.out.print(board[i][j] + "|");
+                    System.out.print(numBoard[i][j].value + "|");
                 } else{
-                    System.out.print(board[i][j] + " ");
+                    System.out.print(numBoard[i][j].value + " ");
                 }
             }
             if(i == 2 || i == 5){
                 System.out.println();
-                System.out.println("|| - - - - - - - - ||");
+                System.out.println("|-----+-----+-----|");
             } else{
                 System.out.println();
             }
         }
-        System.out.println("- - - - - - - - - - -");
+        System.out.println("-------------------");
+    }
+    
+    public static void printConflicts(Map<Integer, List<Integer>> confList, int rows, int cols){
+        List<Integer> confls = new ArrayList<Integer>();
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                confls = confList.get(i*cols+j);
+                System.out.println("conflicts for (" + i + ", " + j + "): " + confls);
+                
+                boolean tempCheck = false;
+                System.out.println("-------------------");
+                for(int k = 0; k < rows; k++){
+                    for(int l = 0; l < cols; l++){
+                        if(l == 0){
+                            System.out.print("|");
+                        }
+                        if(l == cols-1){
+                            for(int x: confls){
+                                if(k*cols+l == x){
+                                    System.out.print((k*cols+l) +"|");
+                                    tempCheck = true;
+                                }
+                            }
+                            if(tempCheck == false)
+                                System.out.print("X|");
+                            tempCheck = false;
+                        } else if(l == 2 || l == 5){
+                            for(int x: confls){
+                                if(k*cols+l == x){
+                                    System.out.print((k*cols+l) +"|");
+                                    tempCheck = true;
+                                }
+                            }
+                            if(tempCheck == false)
+                                System.out.print("X|");
+                            tempCheck = false;
+                        } else{
+                            for(int x: confls){
+                                if(k*cols+l == x){
+                                    System.out.print((k*cols+l) +" ");
+                                    tempCheck = true;
+                                }
+                            }
+                            if(tempCheck == false)
+                                System.out.print("X ");
+                            tempCheck = false;
+                        }
+                    }
+                    if(k == 2 || k == 5){
+                        System.out.println();
+                        System.out.println("|-----+-----+-----|");
+                    } else{
+                        System.out.println();
+                    }
+                }
+                System.out.println("-------------------");
+            }
+        }
     }
 
     public static void main(String[] args) {
+        SudokuSolverFinal ssf1 = new SudokuSolverFinal();
         String fileName = "";
 	try{
             fileName = args[0];
@@ -57,8 +154,6 @@ public class SudokuSolverFinal {
 	int numRows = 0;
         int numCols = 0;
         int n = 0;
-        int [][] board;
-        int o = 0;
 	
         while(fileScnr.hasNext()){
             //Detect board size
@@ -80,10 +175,24 @@ public class SudokuSolverFinal {
             
             numRows = n;
             numCols = n;
-            board = new int [numRows][numCols];
+            int sqrtN = (int)Math.sqrt(n);
+            int curVal = 0;
+            board = new square [numRows][numCols];
+            List<Integer> tempList = new ArrayList<Integer>();
+            for(int i = 0; i < numRows; i++){
+                tempList.add(i);
+            }
+            conflictsList = new HashMap<Integer, List<Integer>>();
+
+             
+            
             
             for(int i = 0; i < numRows; i++){
                 for(int j = 0; j < numCols; j++){
+                    //System.out.print("i: " + i + ", j: " + j + " = ");
+                    square s1 = ssf1.new square(-1,-1,tempList);
+                    
+                    board[i][j] = s1;
                     if(fileScnr.hasNext()){  //might be able to remove this if stmt
                         tempLine = fileScnr.next();
                         tempLine = tempLine.trim();
@@ -92,11 +201,50 @@ public class SudokuSolverFinal {
                         tempLine = fileScnr.next();
                         tempLine = tempLine.trim();
                     }
-                    board[i][j]= Integer.parseInt(tempLine);
+                    curVal = Integer.parseInt(tempLine);
+                    board[i][j].setValue(curVal);// = Integer.parseInt(tempLine);
+                    board[i][j].setPosition(i*numCols + j);
+                    if(curVal != 0){
+                        List<Integer> tempList2 = new ArrayList<Integer>();
+                        tempList2.clear();
+                        tempList2.add(curVal);
+                        board[i][j].setOptions(tempList2);
+                    } else{
+                        board[i][j].setOptions(tempList);
+                    }
                 }
+                //System.out.println();
             }
 
+
+            int rowQuad = -1;
+            int colQuad = -1;
+            for(int i = 0; i < numRows; i++){
+                for(int j = 0; j < numCols; j++){
+                    List<Integer> tempConflicts = new ArrayList<Integer>();
+                    tempConflicts.clear();
+                    for(int k = 0; k < numCols; k++){
+                        if(j != k)
+                            tempConflicts.add(i*numCols + k);
+                        if(i != k)
+                            tempConflicts.add(k*numRows + j);
+                    }
+                    rowQuad = (i/sqrtN);
+                    colQuad = (j/sqrtN);
+                    
+                    for(int k = rowQuad*sqrtN; k < (rowQuad+1)*sqrtN; k++){
+                        for(int l = colQuad*sqrtN; l < (colQuad+1)*sqrtN; l++){
+                            if(i != k && j != l)
+                                tempConflicts.add(k*numCols + l);
+                        }
+                    }
+                    conflictsList.put(i*numCols+j, tempConflicts);
+                }
+            }
+            
             printBoard(board, numRows, numCols);
+            //printConflicts(conflictsList, numRows, numCols);
+            
             
         }
     }
